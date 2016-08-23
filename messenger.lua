bdb00f506ad801036e8b3f93e7f0157d6a4d2d1e
GreenCommunications <alexander.m.rankin@gmail.com>
2016-08-23 15:46:24 -0400

    Config start

--- messenger.lua
+++ messenger.lua
@@ -1,68 +1,27 @@
--- Gets functions ready for use
-function Setup()
-  term.clear()
-  term.setBackgroundColor(colors.black)
-  term.clear()
-  term.setCursorPos(1,1)
-  term.setTextColor(colors.white)
-  textutils.slowPrint("Welcome to Messaging!")
-  textutils.slowPrint("Before we can continue, we have to setup the app for you")
-  os.sleep(2)
-  term.clear()
+os.pullEvent = os.pullEventRaw
+
+if not fs.exists(".CHATCONFIG")
+  local toAppend = {}
+
   term.setBackgroundColor(colors.gray)
-  term.clear()
-  term.setCursorPos(1,1)
-  print("Please type in a color theme")
-  colortheme = read()
-  print("Applying...")
-  shell.run("mkdir","config")
-  shell.run("cd","config")
-  file = fs.open("/config/color", "w")
-  file.write(colortheme)
-  file.close()
-  term.clear()
-  term.setCursorPos(1,1)
-  textutils.slowPrint("Checking for rednet...")
-  rednetCheckSetup()
-end
-  
-function rednetCheckSetup()
-  local modemSide = getDeviceSide("modem")
-   if (modemSide) then
-     print("Rednet Found")
-     textutils.slowPrint("Welp, this is the end of Setup! I really hope you enjoy this app! Please report all bugs on the Issues fourms! Thank You and have a Nice Day!")
-     os.sleep(3)
-     fs.delete("/status")
-     file = fs.open("/status", "w")
-     file.write("complete")
-     file.exit()
-     Load()
-  else
-    print("Please attach a modem. Press Enter to retry")
-    rednetCheckSetup()
-  end
-end
- 
 
-function Load()
   term.clear()
-  print("Loaded")
-  print("This app is under development and doesn't work yet, Sorry!")
-end
 
--- Program Starts Here
-term.clear()
-if fs.exists("/status") == false then
-  file = fs.open("/status", "w")
-  file.write("nosetup")
-  file.close()
-  Setup()
-elseif fs.exists("/status") == true then
-  file = fs.open("/status", "r")
-  result = file.readLine()
-  if result == "nosetup" then
-    Setup()
-  elseif result == "complete" then
-    Load()
-  end
+  paintutils.drawLine(1, 1, x, 1, colors.lightGray)
+
+  term.setTextColor(colors.white)
+
+  term.setCursorPos(1, 1)
+
+  print("Messenger Configuration")
+
+  term.setBackgroundColor(colors.gray)
+
+  term.setCursorPos(1, 3)
+
+  print("Enter your username:")
+
+  term.setCursorPos(1, 4)
+
+  toAppend["username"] = read()
 end
